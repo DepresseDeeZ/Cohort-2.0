42922001cb2c67f9bb0994f624ef0e0f72282f4c
Umang Raval <umangraval0365@gmail.com>
2024-09-25 17:22:07 +0530

    created a small authenticated course sellign backend to run

--- Week 4/Week 4.1/calculateSum/script1.js
+++ Week 4/Week 4.1/calculateSum/script1.js
@@ -4,22 +4,21 @@ function populateDivPrint() {
   console.log(el);
   el.innerHTML = "hello";
 }
-//this will create a temporary id for timeout 
+//this will create a temporary id for timeout
 let timeout;
-function dePopulateDiv(){
+function dePopulateDiv() {
   //debouncing and throttling
   //this will delay the call of populate until the div will not call for 100ms
-  clearTimeout(timeout)
-  timeout = setTimeout(()=>{
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
     populateDiv();
-  },1000);//simple solutions
+  }, 1000); //simple solutions
 
   //advance one
   // how do you cancel a clock
   //clearTimeot will clear the clock
   // clearTimeout(timeout)
-  //now to debounce this 
-
+  //now to debounce this
 }
 
 async function populateDiv() {
@@ -27,12 +26,12 @@ async function populateDiv() {
   const a = parseInt(document.getElementById("f1").value);
   const b = parseInt(document.getElementById("f2").value);
   const el = document.getElementById("finalSum");
-  //using variable to sum it 
+  //using variable to sum it
   // el.innerHTML = a+b;
 
   //using fetch to calcualte using fetch method
   const response = await fetch("http://localhost:8080/sum?a=" + a + "&b=" + b);
-  const data  = await response.text();
-  console.log(typeof data)
-  el.innerHTML = data
+  const data = await response.text();
+  console.log(typeof data);
+  el.innerHTML = data;
 }
--- assignments-master/week-3/03-mongo/routes/user.js
+++ assignments-master/week-3/03-mongo/routes/user.js
@@ -1,22 +1,77 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    await User.create({
+        username:username,
+        password:password
+    })
+    res.json({
+        message:'User created successfully'
+    })
+
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+    const getCourses = await Course.find({});
+    res.json({
+        courses:getCourses
+    })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware,async (req, res) => {
     // Implement course purchase logic
+    const id = req.params.courseId;
+    const username  =req.headers.username;
+    try {
+        await User.updateOne({
+            username:username
+        },{
+            //this is wrong
+            // purchasedCourses:{
+            //     "$push":id
+            // }
+            //here it was not able to convert this 
+
+            //the right way  
+            "$push":{
+                purchasedCourses:id
+            }
+
+        });  
+    } catch (error) {
+        console.log(error);
+        
+    }
+    res.json({
+        message:"Purchase Complete"
+    })
+ 
+
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    const user = await User.findOne({
+        username: req.headers.username,
+    })
+    // console.log(user.purchasedCourses);
+    const course = await Course.find({
+        _id:{
+            "$in":user.purchasedCourses //to find in where array type shii
+        }
+    })
+    res.status(200).json({
+        courses:course
+    })
+
 });
 
 module.exports = router
