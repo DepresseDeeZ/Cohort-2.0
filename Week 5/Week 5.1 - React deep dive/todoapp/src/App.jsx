a1244a52530489344b427e82c637e624ac80f9c5
Umang Raval <umangraval0365@gmail.com>
2024-10-04 19:07:58 +0530

    simple todo

---
+++ Week 5/Week 5.1 - React deep dive/todoapp/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import "./App.css"
+function App() {
+  const [todos,setTodos] = useState([{
+    title:"Got to Gym",
+    description:"go to gym form 7-10",
+    completed:false
+  },{
+    title:"Do DSA",
+    description:"Do DSA from 8-10",
+    completed:true
+  },{
+    title:"Do React",
+    description:"Do React from 10-12",
+    completed:false
+  },{
+    title:"Do Swimminh",
+    description:"Swimming from 10-12",
+    completed:false
+  }]);
+
+  function addTodo(){
+    setTodos([...todos,{
+      title:"New Todo",
+      description:"New Description",
+      completed:false
+    }])
+  }
+
+
+  return (
+    <div>
+      {/* {JSON.stringify(todos)} */}
+      {/* using brute force */}
+      {/* <Todo title={todos[0].title} description={todos[0].description} ></Todo>
+      <Todo title={todos[1].title} description={todos[1].description} ></Todo> */}
+
+      {/* using map */}
+      <button onClick={addTodo}>add todos</button>
+      {
+        todos.map((todo,index)=>{
+          return <Todo key={index} title={todo.title} description={todo.description}></Todo>
+        })
+      }
+    </div>
+  )
+}
+
+function Todo(props){
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+    </div>
+  )
+}
+
+export default App
