535aca6267a876557b6be421ecafa7cf80922275
Umang Raval <umangraval0365@gmail.com>
2024-10-07 17:37:07 +0530

    todo app phase 2 backend completed

---
+++ Week 5/Week 5.2 -  React Todo Project/backend/db.js
@@ -0,0 +1,34 @@
+//create the schema for our project
+const mongoose =  require('mongoose');
+const { boolean } = require('zod');
+
+/*
+    Todo {
+        title:string,
+        description:string,
+        completedLboolean
+    
+    }
+
+*/
+
+//using mongodb url
+// mongodb+srv://umangraval749:efiShv9O7BjxgALE@cluster0.2wifg.mongodb.net/todos
+
+//connect the db to the mongo db server
+mongoose.connect('mongodb+srv://umangraval749:efiShv9O7BjxgALE@cluster0.2wifg.mongodb.net/todos');
+
+//creating a schema 
+const todoSchema = mongoose.Schema({
+    title : String,
+    description : String,
+    completed : Boolean
+})
+
+//creating a model 
+const todo = mongoose.model('todos',todoSchema);
+
+//exporting the module
+module.exports = {
+    todo
+}
--- Week 5/Week 5.2 -  React Todo Project/backend/index.js
+++ Week 5/Week 5.2 -  React Todo Project/backend/index.js
@@ -3,21 +3,64 @@
 //use zod for validation
 
 const express =  require('express');
+const { createTodo, updateTodo } = require('./types');
+const { todo } = require('./db');
 const app = express();
-
 
 
 app.use(express.json());
 
-app.get("/todo",(req,res)=>{
+app.get("/todo",async (req,res)=>{
+
+    const todos = await todo.find({});
+    console.log(todos)
+
+    res.json({
+        todos
+    })
+    
 
 })
-app.post("/todos",(req,res)=>{
 
+
+app.post("/todos", async (req,res)=>{
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            mgs:"You sent the wrong inputs",
+        })
+        return;
+    }
+
+    //put it in mongodb
+    await todo.create({
+        title : createPayload.title,
+        description : createPayload.description,
+        completed: false
+    })
+
+    res.json({
+        msg:"todo Created"
+    })
 })
 
-app.put("/completed",(req,res)=>{
 
+app.put("/completed",async (req,res)=>{
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"You sent the worng inputs",
+        })
+        return;
+    }
+
+    await todo.update({
+        _id: req.body.id
+    },{
+        completed:true
+    })
 })
 
 app.listen(3000);
